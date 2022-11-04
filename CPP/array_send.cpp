dad2f44afc6d78600c668edc39e498fb07fb15b5
tackyoung.kwon <tackyoung.kwon@imts.co.kr>
2022-11-04 16:52:04 +0900

    갱신

--- CPP/array_send.cpp
+++ CPP/array_send.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 
-void Func(std::string *arr, int length)
+enum Enu
 {
-    for (int i = 0; i < length; i++)
-    {
-        std::cout << *(arr + i);
-    }
-    std::cout << std::endl;
-}
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE
+};
 
 int main()
 {
-    std::string str_arr[] = {"hello", ", ", "World", "!"};
-    int length = sizeof(str_arr) / sizeof(std::string);
-
-    Func(str_arr, length);
+    int n1 = 1;
+    switch (n1)
+    {
+    case ONE:
+        std::cout << "one" << std::endl;
+        break;
     
+    default:
+        std::cout << "false" << std::endl;
+        break;
+    }    
     return 0;
 }
