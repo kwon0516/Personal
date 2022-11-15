12524170f152d7c3c6b4ba181dbbbc78099db013
tackyoung.kwon <tackyoung.kwon@imts.co.kr>
2022-11-15 18:23:10 +0900

    객체, 클래스 시작

--- CPP/0_test.cpp
+++ CPP/0_test.cpp
@@ -1,18 +1,45 @@
 #include <iostream>
 
-void print(int *arr, int length)
+struct Animal
 {
-    for (int i = 0; i != length; i++)
+    std::string name;
+    int age;
+    int health = 100;
+    int food = 100;
+    int clean = 100;
+
+    void Play()
     {
-        std::cout << arr[i] << std::endl;
+        health += 10;
+        food -= 20;
+        clean -= 10;
     }
-}
+
+    void ShowState()
+    {
+        std::cout << "name : " << name << std::endl;
+        std::cout << "age : " << age << std::endl;
+        std::cout << "health : " << health << std::endl;
+        std::cout << "food : " << food << std::endl;
+        std::cout << "clean : " << clean << std::endl;
+    }
+};
+
 
 int main()
 {
-    int arr[3] = {1, 3, 5};
+    Animal poppy;
 
-    print(arr, 3);
+    Animal *ptr_arr[10];
+
+    poppy.name = "poppy";
+    poppy.age = 10;
+
+    poppy.Play();
+
+    poppy.ShowState();
+
     
+
     return 0;
 }
--- CPP/Function_ptr.cpp
+++ CPP/Function_ptr.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 
-void Func()
+void func1(int n1, int n2)
 {
-    std::cout << "Function pointer test!" << std::endl;
+    std::cout << n1 + n2 << std::endl;
+}
+
+void func2(int n1, int n2)
+{
+    std::cout << n1 * n2 << std::endl;
 }
 
 int main()
 {
-    void (*ptr_func)() = Func;
-
-    (*ptr_func)();
+    void (*ptr[2])(int, int) = {&func1, &func2};
+    
+    (*ptr[0])(2, 5);
+    ptr[0](2, 5);
     
     return 0;
 }
