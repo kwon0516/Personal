b10dd3bb9db917576b1a025c717ad75a0bfc8542
tackyoung.kwon <tackyoung.kwon@imts.co.kr>
2022-12-27 18:38:28 +0900

    스타크래프트 마린

--- CPP/starcraft.cpp
+++ CPP/starcraft.cpp
@@ -64,6 +64,21 @@ void Marine::show_status()
 int main()
 {
 	Marine* marines[100];
+
+    marines[0] = new Marine(2, 3);
+    marines[1] = new Marine(3, 5);
+
+    // marines[0]->show_status();
+    // marines[1]->show_status();
+
+    int arr[10] = {0, };
+
+    for (int i : arr)
+    {
+        std::cout << i << " ";
+    }
+
+    std::cout << std::endl;
 	
 	return 0;
 }
