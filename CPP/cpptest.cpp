5d0e5b1ce89938ffb75a9951a6d16b7853b5bf58
[kwon0516] <[chounkal3@gmail.com]>
2023-02-23 23:32:09 +0900

    task.json 추가

--- CPP/cpptest.cpp
+++ CPP/cpptest.cpp
@@ -1,16 +1,8 @@
-#include <stdio.h>
+#include <iostream>
 
 int main()
 {
-    int a = 10;
-    int b = 20;
-    int c;
-    c = a + b;
-    printf("a + b = %d\n", c);
-    for (int i = 1; i < 10; i++)
-    {
-        printf("C = %d\n", c-i);
-    }
-
+    std::cout << "Hello" << std::endl;
+    
     return 0;
 }
