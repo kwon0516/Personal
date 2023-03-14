04e233409bddb420843da62bb654e35b2fd2ff08
tackyoung.kwon <tackyoung.kwon@imts.co.kr>
2023-03-14 10:21:47 +0900

    AutoHotKey 최종버전

--- CPP/0_test.cpp
+++ CPP/0_test.cpp
@@ -1,37 +1,13 @@
 #include <iostream>
 
-class C
-{
-	public:
-	C();
-	~C();
-	void DefaultFunction(bool b = false);
-};
-
-C::C()
-{
-	std::cout << "Create!" << std::endl;
-}
-
-C::~C()
-{
-	std::cout << "Destroy!" << std::endl;
-}
-
-void C::DefaultFunction(bool b)
-{
-	if (b) std::cout << "True" << std::endl;
-	else std::cout << "False" << std::endl;
-}
-
 int main()
 {
-	C c;
+	for (int i = 0; i != 9; i++)
+	{
+		std::cout << i << ' ';
+	}
+
+	std::cout << std::endl;
 	
-	c.DefaultFunction();
-	c.DefaultFunction(true);
-	
-	std::cout << '1' << std::endl;
 	return 0;
-	std::cout << '2' << std::endl;
 }
--- Python/AutoHotKey.py
+++ Python/AutoHotKey.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtTest import *
 from PyQt5 import uic
+from PyQt5 import QtGui
 import pyautogui
 
 form_class = uic.loadUiType("E:/0_Git/Personal/Python/AutoKey.ui")[0]
@@ -11,15 +12,38 @@ class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.setWindowTitle("AutoHotKey")
+        self.setWindowIcon(QtGui.QIcon('E:/0_Git/Personal/Python/AutoIcon-removebg-preview.png'))
+        self.setWindowTitle('AutoHotKey')
         self.setFixedSize(QSize(295, 195))
-    
+        
         self.pushButton_Start.clicked.connect(self.ClickedPushButton)
         self.pushButton_Stop.clicked.connect(self.CLickedStopButton)
         
         self.status_run = False
         self.key = ""
         self.cycle = 0
+        
+        self.trayIcon = QSystemTrayIcon(QtGui.QIcon('E:/0_Git/Personal/Python/AutoIcon-removebg-preview.png'), app)
+        self.trayIcon.setToolTip("상태 : Stop")
+        self.TrayInit()        
+
+        pyautogui.FAILSAFE = False
+    
+    def TrayInit(self):
+        show_action = QAction("Show", self)
+        hide_action = QAction("Hide", self)
+        show_action.triggered.connect(self.show)
+        hide_action.triggered.connect(self.hide)
+        tray_menu = QMenu()
+        tray_menu.addAction(show_action)
+        tray_menu.addAction(hide_action)
+        self.trayIcon.setContextMenu(tray_menu)
+        
+        self.trayIcon.show()
+    
+    def closeEvent(self, event):
+        self.trayIcon.hide()
+        sys.exit()
         
     def ClickedPushButton(self):
         if self.status_run == True:
@@ -42,16 +66,18 @@ class WindowClass(QMainWindow, form_class):
         else:
             self.AutoKeyStart()
 
-    def AutoKeyStart(self):
+    def AutoKeyStart(self):        
         self.cycle *= 60000
         self.comboBox_SelectKey.setEnabled(False)
         self.lineEdit_Cycle.setEnabled(False)
         self.status_run = True
+        self.trayIcon.setToolTip("상태 : Run")
         self.label_Status.setText("상태 : Run")
         
         while self.status_run:
             pyautogui.keyDown(self.key)
             pyautogui.keyUp(self.key)
+            QTest.qWait(100)
             pyautogui.keyDown(self.key)
             pyautogui.keyUp(self.key)
             QTest.qWait(self.cycle)
@@ -60,6 +86,7 @@ class WindowClass(QMainWindow, form_class):
         self.comboBox_SelectKey.setEnabled(True)
         self.lineEdit_Cycle.setEnabled(True)
         self.status_run = False
+        self.trayIcon.setToolTip("상태 : Stop")
         self.label_Status.setText("상태 : Stop")
 
 
