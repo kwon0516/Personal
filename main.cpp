4e3f2af17c71aaadd48806c9f35fe3e772623ada
TackYoung <tackyoung.kwon@imts.co.kr>
2023-07-06 14:24:17 +0900

    [ AutoHotKey ] 1. 디폴트 시간 3분으로 변경 2. 배포 커맨드 예시 추가

--- main.cpp
+++ main.cpp
@@ -9,3 +9,20 @@ int main()
     
     return 0;
 }
+
+if (bSite)
+   {
+      strInspectPath.Format(_T("\\\\192.168.100.17\\VP_Image\\%s\\%s\\Main\\"), strFolder/*strYear, strMonth, strDay*/, strCellID);
+   }
+   else
+   {
+      CString strNasIP;
+      if (m_nSelectLine == 1)
+         strNasIP = _T("192.168.100.100");
+      else
+         strNasIP = _T("192.168.100.200");
+
+      strInspectPath.Format(_T("\\\\%s\\nas_storage\\VP_Image\\%s\\%s\\Main\\"), strNasIP, strFolder/*strYear, strMonth, strDay*/, strCellID);
+   }
+
+   CreateFolder(strInspectPath);
