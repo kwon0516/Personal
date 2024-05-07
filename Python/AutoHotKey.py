abd86ac5164fcebd2f6c7cf2cb58a1a3eecec1e4
DESKTOP-4MU1PVK\user <TackYoung.Kwon_Desktop>
2024-05-07 19:40:46 +0900

    1. 핫코너 기능구현 완료 2. ini 파일 추가해야 함

--- Python/AutoHotKey.py
+++ Python/AutoHotKey.py
@@ -8,25 +8,62 @@ from PyQt5.QtCore import QEvent, QObject, QTimer, QTime
 import pyautogui
 from pynput import keyboard
 
-form_class = uic.loadUiType("E:/Git/Personal/Python/AutoKey.ui")[0]
+form_class = uic.loadUiType("D:/Git/Personal/Python/AutoKey.ui")[0]
+# form_class = uic.loadUiType("E:/Git/Personal/Python/AutoKey.ui")[0]
+form_class2 = uic.loadUiType("D:/Git/Personal/Python/InputKey.ui")[0]
+# form_class2 = uic.loadUiType("E:/Git/Personal/Python/InputKey.ui")[0]
 
 class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
+        
+        self.specialKey = {16777216: 'esc', 16777252: 'capslock', 16777248: 'shift',
+                           16777249: 'ctrl', 16777250: 'win', 16777251: 'alt', 16777217: 'tab',
+                           16777234: 'left', 16777235: 'up', 16777236: 'right', 16777237: 'down'}
+        
+        self.status_run = False
+        self.key = ""
+        self.cycle = 0
+        
+        # =============================================================================================================
+        
+        self.win2 = WindowClass2()
+        self.win2.setWindowTitle(' ')
+        self.win2.setFixedSize(QSize(150, 100))
+        self.win2.setWindowFlags(Qt.WindowTitleHint | Qt.WindowCloseButtonHint)
+        
+        # =============================================================================================================
         
         self.Thread_HotCorner = Thread(self)
         self.Thread_HotCorner.start()
         self.setupUi(self)
         self.setWindowIcon(QtGui.QIcon('E:/Git/Personal/Python/AutoIcon-removebg-preview.png'))
         self.setWindowTitle('AutoHotKey')
-        self.setFixedSize(QSize(295, 195))
+        self.setFixedSize(QSize(295, 380))
+        
+        # =============================================================================================================
         
         self.pushButton_Start.clicked.connect(self.ClickedPushButton)
         self.pushButton_Stop.clicked.connect(self.CLickedStopButton)
+        self.pushButton_hotkey_1_0.clicked.connect(lambda: self.TTT(0, 0))
+        self.pushButton_hotkey_1_1.clicked.connect(lambda: self.TTT(0, 1))
+        self.pushButton_hotkey_1_2.clicked.connect(lambda: self.TTT(0, 2))
+        self.pushButton_hotkey_2_0.clicked.connect(lambda: self.TTT(1, 0))
+        self.pushButton_hotkey_2_1.clicked.connect(lambda: self.TTT(1, 1))
+        self.pushButton_hotkey_2_2.clicked.connect(lambda: self.TTT(1, 2))
+        self.pushButton_hotkey_3_0.clicked.connect(lambda: self.TTT(2, 0))
+        self.pushButton_hotkey_3_1.clicked.connect(lambda: self.TTT(2, 1))
+        self.pushButton_hotkey_3_2.clicked.connect(lambda: self.TTT(2, 2))
+        self.pushButton_hotkey_4_0.clicked.connect(lambda: self.TTT(3, 0))
+        self.pushButton_hotkey_4_1.clicked.connect(lambda: self.TTT(3, 1))
+        self.pushButton_hotkey_4_2.clicked.connect(lambda: self.TTT(3, 2))
         
-        self.status_run = False
-        self.key = ""
-        self.cycle = 0
+        self.hotKeyList = [[self.pushButton_hotkey_1_0, self.pushButton_hotkey_1_1, self.pushButton_hotkey_1_2],
+                           [self.pushButton_hotkey_2_0, self.pushButton_hotkey_2_1, self.pushButton_hotkey_2_2],
+                           [self.pushButton_hotkey_3_0, self.pushButton_hotkey_3_1, self.pushButton_hotkey_3_2],
+                           [self.pushButton_hotkey_4_0, self.pushButton_hotkey_4_1, self.pushButton_hotkey_4_2]]
+        
+        # =============================================================================================================
         
         self.trayIcon = QSystemTrayIcon(QtGui.QIcon('E:/Git/Personal/Python/AutoIcon-removebg-preview.png'), app)
         self.trayIcon.setToolTip("상태 : Stop")
@@ -61,9 +98,7 @@ class WindowClass(QMainWindow, form_class):
     def DoubleClickedTrayIcon(self, reson):
         if (reson == QSystemTrayIcon.DoubleClick):
             self.showNormal()
-            # if (self.isVisible()):
-            #     self.show()
-    
+
     def changeEvent(self, event):
         if (event.type() == QEvent.WindowStateChange):
             if (self.windowState() & Qt.WindowMinimized):
@@ -119,19 +154,90 @@ class WindowClass(QMainWindow, form_class):
         self.trayIcon.setToolTip("상태 : Stop")
         self.label_Status.setText("상태 : Stop")
     
-    def keyPressEvent(self, e):
-        str;
-        inputASCII = int(e.key())
-        if inputASCII == 16777252:
-            print("Capslock")
-        
-        print(int(e.key()))
-        # print(chr(int(e.key())))
-        
-        # print(type(e.text()))
-        print("===========")
-
+    def TTT(self, hotNum, keyNum):
+        if self.win2.isVisible() == False:
+            self.win2.SetTargetInfo(hotNum, keyNum)
+            self.win2.show()
     
+    def SetInputValue(self, value, hotNum, keyNum):
+        print('1-1', value, hotNum, keyNum)
+        if value < 999:
+            self.hotKeyList[hotNum][keyNum].setText(chr(value))
+        elif value == 99999999:
+            self.hotKeyList[hotNum][keyNum].setText('None')
+        elif value in self.specialKey:
+            self.hotKeyList[hotNum][keyNum].setText(self.specialKey[value])
+    
+    def HotKeyAction(self, hotKeyNum):
+        if hotKeyNum == 0:
+            key1 = self.hotKeyList[0][0].text()
+            key2 = self.hotKeyList[0][1].text()
+            key3 = self.hotKeyList[0][2].text()
+        elif hotKeyNum == 1:
+            key1 = self.hotKeyList[1][0].text()
+            key2 = self.hotKeyList[1][1].text()
+            key3 = self.hotKeyList[1][2].text()
+        elif hotKeyNum == 2:
+            key1 = self.hotKeyList[2][0].text()
+            key2 = self.hotKeyList[2][1].text()
+            key3 = self.hotKeyList[2][2].text()
+        elif hotKeyNum == 3:
+            key1 = self.hotKeyList[3][0].text()
+            key2 = self.hotKeyList[3][1].text()
+            key3 = self.hotKeyList[3][2].text()
+        
+        if key1 != "none":
+            pyautogui.keyDown(key1)
+        if key2 != "none":
+            pyautogui.keyDown(key2)
+        if key3 != "none":
+            pyautogui.keyDown(key3)
+        
+        if key1 != "none":
+            pyautogui.keyUp(key1)
+        if key2 != "none":
+            pyautogui.keyUp(key2)
+        if key3 != "none":
+            pyautogui.keyUp(key3)
+        
+        return
+
+class WindowClass2(QMainWindow, form_class2):
+    def __init__(self):
+        super().__init__()
+    
+        self.label_inputkey_title = QLabel(self)
+        self.label_inputkey_title.setGeometry(20, 0, 100, 50)
+        self.label_inputkey_title.setText('키를 입력하세요')
+        self.label_inputkey_title.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        
+        self.Button_none = QPushButton(self)
+        self.Button_none.setGeometry(50, 50, 50, 25)
+        self.Button_none.setText('None')
+    
+        self.hotNum = 0
+        self.keyNum = 0
+        self.inputASCII = 0
+        
+        self.Button_none.clicked.connect(lambda: self.SendInputValue(99999999))
+    
+    def keyPressEvent(self, e):
+        str
+        self.inputASCII = int(e.key())
+
+        self.SendInputValue()
+    
+    def SetTargetInfo(self, hotNum, keyNum):
+        self.hotNum = hotNum
+        self.keyNum = keyNum
+    
+    def SendInputValue(self, noneValue = 0):
+        if noneValue == 99999999:
+            self.inputASCII = 99999999
+            
+        myWindow.SetInputValue(self.inputASCII, self.hotNum, self.keyNum)
+        self.close()
+
 class Thread(QThread):
     def __init__(self, parent):
         super().__init__(parent)
@@ -157,8 +263,11 @@ class Thread(QThread):
             
             self.curX = pyautogui.position().x
             self.curY = pyautogui.position().y
+            
+            # print(self.rect.width(), self.rect.height())
+            print(self.curX, self.curY)
 
-            if (self.curX > self.zero and self.curY > self.zero):
+            if ((self.curX != self.zero and self.curX != self.width) or (self.curY != self.zero and self.curY != self.height)):
                 self.actionFlag = True
             elif (self.actionFlag and self.curX == self.zero and self.curY == self.zero):
                 self.CurPosLeftTop()
@@ -176,37 +285,25 @@ class Thread(QThread):
     def StopThread(self):
         self.ThreadFlag = False
 
-    # Chrome
+    # 1
     def CurPosLeftTop(self):
-            pyautogui.keyDown("winleft")
-            pyautogui.keyDown("1")
-            pyautogui.keyUp("1")
-            pyautogui.keyUp("winleft")
+        myWindow.HotKeyAction(0)
         
-    # Visual Studio
+    # 2
     def CurPosRightTop(self):
-        pyautogui.keyDown("winleft")
-        pyautogui.keyDown("2")
-        pyautogui.keyUp("2")
-        pyautogui.keyUp("winleft")
+        myWindow.HotKeyAction(1)
 
-    # SoruceTree
+    # 3
     def CurPosLeftBottom(self):
-            pyautogui.keyDown("winleft")
-            pyautogui.keyDown("4")
-            pyautogui.keyUp("4")
-            pyautogui.keyUp("winleft")
+        myWindow.HotKeyAction(2)
     
-    # Go Desktop
+    # 4
     def CurPosRightBottom(self):
-        pyautogui.keyDown("winleft")
-        pyautogui.keyDown("d")
-        pyautogui.keyUp("d")
-        pyautogui.keyUp("winleft")
+        myWindow.HotKeyAction(3)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv) 
-    myWindow = WindowClass() 
+    myWindow = WindowClass()
     myWindow.show()
     app.exec_()
